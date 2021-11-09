f3eb414c9e8cb288d1d51c952006bb5a81ae34ab
Romi22 <60867863+Romi22@users.noreply.github.com>
2021-11-09 22:37:26 +0530

    Update Two Sum.java

--- Arrays/Two Sum.java
+++ Arrays/Two Sum.java
@@ -31,3 +31,29 @@ class Solution {
         return arr;
     }
 }
+
+
+//BRUTE FORCE APPROACH
+
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int arr[] = new int[2];
+       int k = 0;
+        
+        for(int i = 0; i < nums.length-1; i++)
+        {
+            for(int j = i+1; j < nums.length; j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    arr[k]=i;
+                    arr[k+1]=j;
+                    break;
+                }
+            }
+        }
+        
+        return arr; 
+    }*/
+}
