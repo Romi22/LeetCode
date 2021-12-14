8a59efb942701ca1d9f2a68a5b8c21a1782396c1
Romi22 <60867863+Romi22@users.noreply.github.com>
2021-12-14 16:12:56 +0530

    Update Remove Duplicates from Sorted Array.java

--- Arrays/Remove Duplicates from Sorted Array.java
+++ Arrays/Remove Duplicates from Sorted Array.java
@@ -19,7 +19,7 @@ class Solution {
     }
 }
 
-//2. Using LinkedHashSet (maintains the insertion order )
+//2. Using LinkedHashSet (maintains the insertion order ) [Takes more memory]
 class Solution {
     public int removeDuplicates(int[] nums) {
         
@@ -40,7 +40,7 @@ class Solution {
     }
 }
 
-//3. COUNTING SORT (effective when array has negative values) also constraint is within range
+//3. COUNTING SORT (effective when array has negative values) also constraint is within range  [Takes more memory]
 Constraint : -100 <= nums[i] <= 100, therefore we need an array of size 201( 100 -ve no.s + 0 + 100 +ve no.s )
       Since array index cant be negative we need to shift the negative elements i.e. if -100 to make it a positive no. we must add 100 that means if -100 is the no. 
     it's occurences will be stored at index -100+100 = 0 , so for storing the occurences of 0 we add it at index 100 i.e. count[nums[0]+100].
@@ -76,6 +76,24 @@ class Solution {
               //say 0 will be stored at 100th index to get the value 0 , subtract -100 (as on line 54 indexes are incremented by 100
                 nums[k++] = i-100;
                 
+            }
+        }
+        
+        return k;
+    }
+}
+
+
+//Less Memory more optimised approach
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int k = 0;
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i==0 || (nums[i] != nums[i-1])) {
+                nums[k++] = nums[i];
             }
         }
         
