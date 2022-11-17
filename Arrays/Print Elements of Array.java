8d79abfcc086cd3038cfb30c59fb4c7c43c508e6
Romi22 <60867863+Romi22@users.noreply.github.com>
2022-11-17 08:30:37 +0530

    Create Print Elements of Array.java

---
+++ Arrays/Print Elements of Array.java
@@ -0,0 +1,12 @@
+//https://practice.geeksforgeeks.org/problems/print-elements-of-array4910/1?page=1&difficulty[]=-2&category[]=Arrays&sortBy=submissions
+
+class Solution {
+    //Just print the space seperated array elements
+    void printArray(int arr[], int n) {
+        //   code here
+        for(int i: arr)
+        {
+            System.out.print(i+ " ");
+        }
+    }
+}
