3f34c846377e62e75b3e843e68ab7df8df1b1089
Romi22 <60867863+Romi22@users.noreply.github.com>
2022-11-17 08:27:49 +0530

    Update Swap kth elements.java

--- Arrays/Swap kth elements.java
+++ Arrays/Swap kth elements.java
@@ -3,14 +3,9 @@
 class Solution {
     void swapKth(int arr[], int n, int k) {
         // code here
-        
-        if(n > 1)
-        {
+    
             int temp = arr[k-1];
             arr[k-1] = arr[n-k];
             arr[n-k] = temp;
-        }
-        
     }
-
 }
