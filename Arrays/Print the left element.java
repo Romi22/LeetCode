01077a7605e83c03438730425eaf2f88a29aa9ba
Romi22 <60867863+Romi22@users.noreply.github.com>
2022-11-18 08:16:38 +0530

    Create Print the left element.java

---
+++ Arrays/Print the left element.java
@@ -0,0 +1,23 @@
+//https://practice.geeksforgeeks.org/problems/print-the-left-element2009
+
+class Solution {
+    
+    public long leftElement(long arr[], long n)
+    {
+        // Your code goes here  
+        
+        Arrays.sort(arr);
+        
+        
+           if(n % 2 == 0)
+            {
+                return arr[(int) (n/2)-1];
+            }
+            
+        
+           else
+           {
+               return arr[(int) n/2];
+           }
+    }
+}
