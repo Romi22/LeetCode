02f16075a5eb5b627b0947e125facd152638bb37
Romi22 <60867863+Romi22@users.noreply.github.com>
2022-11-18 22:06:26 +0530

    Create Binary Search.java

---
+++ Arrays/Binary Search.java
@@ -0,0 +1,28 @@
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+         
+        int low = 0;
+        int high = n-1;
+        
+        
+        while(low <= high)
+        {
+            
+            int mid = (low + high)/2;
+            
+            if(arr[mid] == k)
+               return mid;
+               
+            else if(arr[mid] > k)
+                high = mid -1;
+                
+            else if(arr[mid] < k)
+                low = mid + 1;
+                
+            else{}
+        }
+    
+        return -1;
+    }
+}
