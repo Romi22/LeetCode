93fba1c6b4589f1937e4b14ff1124efdd2ceec64
Romi22 <60867863+Romi22@users.noreply.github.com>
2022-11-28 19:51:58 +0530

    Create Trapping Rain Water.java

---
+++ Arrays/Trapping Rain Water.java
@@ -0,0 +1,10 @@
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        // Your code here
+    } 
+}
